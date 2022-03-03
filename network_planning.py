8d70b482ab2f122ecb79656dface3dbee1a699d7
Álvaro García Sánchez <40273074+alvaroelectronico@users.noreply.github.com>
2022-03-03 10:50:06 +0100

    Temp ag (#1)
    * gurobipy model working fine
    
    * Clean vesion of the gurobipy model

--- network_planning.py
+++ network_planning.py
@@ -1,72 +1,25 @@
 import pandas as pd
 import os
 from pyomo.environ import *
-from dask import dataframe as dd
-
-# DATA_PATH =  "D:\Dropbox\\academico\produccion\\articulos\en_curso\\network_planning\datos_entrada\csv\casos_daniele"
-from torch.cuda import init
+import time
+from network_dimensioning.read_data import read_data
 
 
-from werkzeug.debug import Console
-
-DATA_PATH = "..\..\datos_entrada\csv\\analisis_rendimiento\\010"
-case_path = "010_12scope_2020_13_1805"
-# DATA_PATH = "..\..\datos_entrada\csv\casos_daniele"
-# case_path = "5000km2_0"
-pCAPEX_UPGRADE_CEll = 5868
-pCAPEX_NEW_CELL = 11737
-pCAPEX_NEW_SITE = 28855
-pCAPEX_NEW_NODE = 36770
-pOPEX_SITE = 1960
-pOPEX_NODE = 0
-
-
-def read_data(folder_path):network
-    df_initial_capacity = pd.read_csv(os.path.join(folder_path, "capacityi.csv"))
-    df_potential_capacity = pd.read_csv(os.path.join(folder_path, "capacityp.csv"))
-    df_existing_sites = pd.read_csv(os.path.join(folder_path, "existing_sites.csv"))
-    df_potential_sites = pd.read_csv(os.path.join(folder_path, "potential_sites.csv"))
-    df_demand = pd.read_csv(os.path.join(folder_path, "traffic_demand.csv"))
-    df_coverage = pd.read_csv(os.path.join(folder_path, "coverage.csv"))
-
-    df_initial_capacity.set_index(['site_id', 'node', 'cell'], inplace=True)
-    initial_capacity = df_initial_capacity.to_dict(orient="dict")['capacity']
-    df_potential_capacity.set_index(['site_id', 'node', 'cell'], inplace=True)
-    potential_capacity = df_potential_capacity.to_dict(orient="dict")['capacity']
-
-    lots = df_demand.lot_id.unique()
-    nodes = df_demand.node.unique()
-    cells = df_coverage.cell.unique()
-    df_demand.set_index(['lot_id', 'node'], inplace=True)
-    demand = df_demand.to_dict(orient='dict')['demand']
-
-    existing_sites = list(df_existing_sites.site_id.unique())
-    potential_sites = list(df_potential_sites.site_id.unique())
-    sites = existing_sites + potential_sites
-
-    df_coverage.columns
-    df_coverage.set_index(['site_id', 'node', 'cell', 'lot_id'], inplace=True)
-    coverage = list(df_coverage.index)
-
-    return lots, sites, nodes, cells, existing_sites, potential_sites, initial_capacity, potential_capacity, demand, coverage
-
-
-
-# case_path = "001_5"
+# To debug this py file (to be commented or deleted):
+# DATA_PATH = "..\..\datos_entrada\csv\\analisis_rendimiento\\010"
+# case_path = "010_12scope_2020_13_1805"
+# DATA_PATH = "..\..\datos_entrada\csv\\analisis_rendimiento\\001"
+# case_path = "001_1"
+DATA_PATH = "..\..\datos_entrada\csv\casos_daniele"
+case_path = "1km2_0"
 folder_path = os.path.join(DATA_PATH, case_path)
-lots, sites, nodes, cells, existing_sites, potential_sites, initial_capacity, potential_capacity, demand, coverage = read_data(
-    folder_path)
+print("Reading data")
+start_time = time.time()
+lots, sites, nodes, cells, existing_sites, potential_sites, initial_capacity, potential_capacity, demand, \
+coverage, existing_node_in_site, potential_node_in_site, existing_cell_in_site_node, \
+potential_cell_in_site_note, site_cells_lighting_lot_node = read_data(folder_path)
+print("Data read. Time: {}".format(time.time() - start_time))
 
-existing_node_in_site = list(set([(i[0], i[1]) for i in initial_capacity.keys() if initial_capacity[i] > 0]))
-
-potential_node_in_site = [(s, n) for s in sites for n in nodes
-                          if not (s, n) in existing_node_in_site]
-
-existing_cell_in_site_node = [(i[0], i[1], i[2])
-                              for i in initial_capacity.keys() if initial_capacity[i] > 0]
-
-potential_cell_in_site_note = [(s, n, c) for s in sites for n in nodes for c in cells
-                               if not (s, n, c) in existing_cell_in_site_node]
 
 # Definint the abstract model
 model = AbstractModel()
@@ -112,7 +65,6 @@ model.vTotalCost = Var(domain=NonNegativeReals)
 
 # Constraint rules
 def fcDemandFulfilment(model, lot, node):
-    # return model.vTotalCost >= 0
     return sum(model.vTrafficOfCell[site, node, cell, lot]
                for site in model.sSites for cell in model.sCells
                if (site, node, cell, lot) in model.sCoverage) >= model.pTrafficDemand[lot, node]
@@ -177,16 +129,18 @@ def obj_expression(model):
     return model.vTotalCost
 
 
+# def activate_constratins(model):
 # Activating constraints
+
 # model.cDemandFulfilment = Constraint(model.sLotsNodes, rule=fcDemandFulfilment)
-# model.cMinCellCapacity = Constraint(model.sExistingCellsInSitesNodes, rule=fcMinCellCapacity)
-# model.cMaxCellCapacity = Constraint(model.sSites, model.sNodes, model.sCells, rule=fcMaxCellCapacity)
-# model.cMaxCellTraffic = Constraint(model.sSites, model.sNodes, model.sCells, rule=fcMaxCellTraffic)
-# model.cNewCellIfNodeExists = Constraint(model.sPotentialCellsInSitesNodes, rule=fcNewCellIfNodeExists)
-# model.cNewNodeIfSiteExists = Constraint(model.sPotentialNodesInSites, rule=fcNewNodeIfSiteExists)
-# model.cvTotalCost = Constraint(rule=fvTotalCost)
-# model.cEnoughCapacityGlobal = Constraint(model.sNodes, rule=fcEnoughCapacityGlobal)
-# model.cEnoughCapacityPerLot = Constraint(model.sNodes, model.sLots, rule=fcEnoughCapacityPerLot)
+model.cMinCellCapacity = Constraint(model.sExistingCellsInSitesNodes, rule=fcMinCellCapacity)
+model.cMaxCellCapacity = Constraint(model.sSites, model.sNodes, model.sCells, rule=fcMaxCellCapacity)
+model.cMaxCellTraffic = Constraint(model.sSites, model.sNodes, model.sCells, rule=fcMaxCellTraffic)
+model.cNewCellIfNodeExists = Constraint(model.sPotentialCellsInSitesNodes, rule=fcNewCellIfNodeExists)
+model.cNewNodeIfSiteExists = Constraint(model.sPotentialNodesInSites, rule=fcNewNodeIfSiteExists)
+model.cvTotalCost = Constraint(rule=fvTotalCost)
+model.cEnoughCapacityGlobal = Constraint(model.sNodes, rule=fcEnoughCapacityGlobal)
+model.cEnoughCapacityPerLot = Constraint(model.sNodes, model.sLots, rule=fcEnoughCapacityPerLot)
 
 # Objective function
 model.obj_func = Objective(rule=obj_expression)
@@ -211,9 +165,16 @@ input_data = {None: {
 }}
 
 # Creating model instance
+print("Building model")
+start_time = time.time()
 instance = model.create_instance(input_data)
+print("Model build. Time: {}".format(time.time() - start_time))
 
 # Setting the solver
+opt = SolverFactory('gurobi')
+print("Solving")
+results = opt.solve(instance, tee=True)
+print("Solving. Time: {}".format(time.time() - start_time))
 
 print("Total cost: {}".format(instance.vTotalCost.value))
 
---
+++ read_data.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import os
+
+def read_data(folder_path):
+    df_initial_capacity = pd.read_csv(os.path.join(folder_path, "capacityi.csv"))
+    df_potential_capacity = pd.read_csv(os.path.join(folder_path, "capacityp.csv"))
+    df_existing_sites = pd.read_csv(os.path.join(folder_path, "existing_sites.csv"))
+    df_potential_sites = pd.read_csv(os.path.join(folder_path, "potential_sites.csv"))
+    df_demand = pd.read_csv(os.path.join(folder_path, "traffic_demand.csv"))
+    df_coverage = pd.read_csv(os.path.join(folder_path, "coverage.csv"))
+
+    existing_sites = list(df_existing_sites.site_id.unique())
+    potential_sites = list(df_potential_sites.site_id.unique())
+    sites = existing_sites + potential_sites
+    lots = df_demand.lot_id.unique()
+    nodes = df_demand.node.unique()
+    cells = df_coverage.cell.unique()
+
+    df_initial_capacity.set_index(['site_id', 'node', 'cell'], inplace=True)
+    initial_capacity = df_initial_capacity.to_dict(orient="dict")['capacity']
+    df_potential_capacity.set_index(['site_id', 'node', 'cell'], inplace=True)
+    max_capacity = df_potential_capacity.to_dict(orient="dict")['capacity']
+    df_demand.set_index(['lot_id', 'node'], inplace=True)
+    demand = df_demand.to_dict(orient='dict')['demand']
+
+    df_coverage.columns
+    df_coverage2 = df_coverage.set_index(['site_id', 'node', 'cell', 'lot_id'])
+    coverage = list(set(df_coverage2.index))
+    print("coverage read")
+
+    existing_node_in_site = list(set([(i[0], i[1]) for i in initial_capacity.keys() if initial_capacity[i] > 0]))
+
+    potential_node_in_site = [(s, n) for s in sites for n in nodes
+                               if not (s, n) in existing_node_in_site]
+    
+    existing_cell_in_site_node = [(i[0], i[1], i[2])
+                                  for i in initial_capacity.keys() if initial_capacity[i] > 0]
+
+    potential_cell_in_site_node = [i for i in initial_capacity.keys() if not i in existing_cell_in_site_node]
+
+    df_coverage['site_cell'] = list(zip(df_coverage['site_id'], df_coverage['cell']))
+    df_coverage3 = df_coverage.groupby(by=['lot_id', 'node'])['site_cell'].apply(lambda x: x.values.tolist())
+    site_cells_lighting_lot_node = df_coverage3.to_dict()
+    print("coverage read")
+
+    return lots, sites, nodes, cells, existing_sites, potential_sites, initial_capacity, max_capacity, demand, \
+           coverage, existing_node_in_site, potential_node_in_site, existing_cell_in_site_node, potential_cell_in_site_node, \
+           site_cells_lighting_lot_node
